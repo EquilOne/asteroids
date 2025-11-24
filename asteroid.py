bfcf852e20382b1443e766175e16fa1e96a40cb6
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-11-24 06:24:33 -0700

    feat(asteroidfield):add asteroid field generation

--- asteroid.py
+++ asteroid.py
@@ -3,13 +3,13 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
         self.velocity = super().velocity
         self.center = (x, y)
-        self.radius = radius
 
         def draw(self, center, radius):
-            pygame.draw.circle(screen, "white", center, self.radius)
+            pygame.draw.circle(screen, "white", center, self.radius, LINE_WIDTH)
 
         def update(self, dt):
             move_straight = self.position + (self.velocity * dt)
-            self.position = move_straight
+            self.position += move_straight
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -13,7 +15,11 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, drawable)
+    AsteroidField.containers = updatable
     Player.containers = (updatable, drawable)
+    asteroid_field = AsteroidField()
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
