5265e96abd8d97eb9c358ddd21ef3d76ea5b9383
Chase <188378569+EquilOne@users.noreply.github.com>
2025-11-27 08:46:44 -0700

    feat(hud): add further structure for hud

--- hud.py
+++ hud.py
@@ -1,13 +1,20 @@
 import pygame
 import pygame.freetype
 
+default_font = pygame.freetype.get_default_font()
 
-class HUD:
-    def __init__(self, screen, x, y):
-        self.screen = screen
+class HUDElement(pygame.sprite.Sprite):
+    def __init__(self, default_font, x, y):
         self.position = pygame.Vector2(x, y)
-        self.font = pygame.freetype.getO_default_font()
-        self.text = pygame.freetype.SysFont(self.font, 20)
+        self.font = pygame.freetype.SysFont(default_font, 20)
+        self.color = #FFFFFF
 
-    def write_text(self, screen, input, color):
-        self.text.render_to(screen, (self.position.x, self.position.y), input, color)
+    def draw(self):
+    def update(self,)
+
+class Score(HUDElement):
+    def __init__(self, default_font, x, y, text, anchor):
+        super().__init__(default_font, x, y)
+
+
+    # def update():
