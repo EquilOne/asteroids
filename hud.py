faacc9b1b7150b747bcdd78d89cacb5518caf290
Chase <188378569+EquilOne@users.noreply.github.com>
2026-01-02 14:22:50 -0700

    Style(hud.py): add "" to hex color assignment

--- hud.py
+++ hud.py
@@ -3,18 +3,19 @@ import pygame.freetype
 
 default_font = pygame.freetype.get_default_font()
 
+
 class HUDElement(pygame.sprite.Sprite):
     def __init__(self, default_font, x, y):
         self.position = pygame.Vector2(x, y)
         self.font = pygame.freetype.SysFont(default_font, 20)
-        self.color = #FFFFFF
+        self.color = "#FFFFFF"
 
     def draw(self):
-    def update(self,)
+        pass
+
 
 class Score(HUDElement):
     def __init__(self, default_font, x, y, text, anchor):
         super().__init__(default_font, x, y)
-
 
     # def update():
